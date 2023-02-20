d3d632c4db91d0e172694ab3a3741b853c8bfe1b
Ryan S <114497815+rsmith531@users.noreply.github.com>
2023-02-20 11:11:48 -0500

    create class.throwable.js

---
+++ class.throwable.js
@@ -0,0 +1,7 @@
+//     file name: class.throwable.js
+//        author: Ryan Smith
+//  date created: 20 Feb 2023
+// date modified: 20 Feb 2023
+
+// description: Contains an abstract base class for a throwable object, and other
+//              derived classes of specific throwable objects.
