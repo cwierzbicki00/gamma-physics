981a5c769481592408c49e331b542a179adb7d25
Christian Wierzbicki <cwierzbicki@townchest.com>
2023-02-27 16:29:42 -0500

    fix: change load ins for scripts and startGame on load

--- modules/menu.js
+++ modules/menu.js
@@ -5,56 +5,59 @@
 
 // description: basic menu with functioning start button
 
-
 //when button clicked start game
-let startButton = document.getElementsByClassName("btn-start");
+//let startButton = document.getElementsByClassName("btn-start");
 
-startButton[0].addEventListener("click", startGame);
+//startButton[0].addEventListener("click", startGame);
+
+//p5js setup to start game on load
+function setup() {
+  startGame();
+}
 
 function startGame() {
   // game code here
   console.log("game started");
   createCanvas(windowWidth, windowHeight);
-  let gameBall; // create an empty object to store the game ball in
+  let gameBall;
   buildBall(1, gameBall); // TODO replace parameter with appropriate menu selection
 }
 
 function mousePressed() {
-    gameBall.pressed(mouseX, mouseY);
+  gameBall.pressed(mouseX, mouseY);
 }
 
 function mouseReleased() {
-    gameBall.released();
+  gameBall.released();
 }
 
 function draw() {
-    // background(150);
-    // textAlign(CENTER);
-    // textSize(36);
-    // text("Gamma Game", width / 2, height / 3);
-    background(0);
+  // background(150);
+  // textAlign(CENTER);
+  // textSize(36);
+  // text("Gamma Game", width / 2, height / 3);
+  background(0);
 
-    let gravity = createVector(0, 0.2);
-    let weight = p5.Vector.mult(gravity, gameBall.mass);
-    gameBall.applyForce(weight);
+  let gravity = createVector(0, 0.2);
+  let weight = p5.Vector.mult(gravity, gameBall.mass);
+  gameBall.applyForce(weight);
 
-    gameBall.over(mouseX, mouseY);
-    gameBall.update();
-    gameBall.edges();
-    gameBall.show();
+  gameBall.over(mouseX, mouseY);
+  gameBall.update();
+  gameBall.edges();
+  gameBall.show();
 
-    stroke(255);
-    strokeWeight(4);
-    line(width,150,width-75,150);
+  stroke(255);
+  strokeWeight(4);
+  line(width, 150, width - 75, 150);
 }
 
 function buildBall(ballType, _gameBall) {
-    switch (ballType) {
-        case 1: // baseball
-            _gameBall = new throwable(150, height + 150, 0.142);
-            break;
-
-    }
+  switch (ballType) {
+    case 1: // baseball
+      _gameBall = new throwable(150, height + 150, 0.142);
+      break;
+  }
 }
 
 // function setup() {
