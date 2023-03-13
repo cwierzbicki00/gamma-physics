cc9bdc4398798beceeeb5d94b3551909f873cb7b
nickweber <nweber2001@icloud.com>
2023-03-12 23:37:22 -0400

    added gameScreen page and styling to app

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route('/')
+@app.route('/index.html')
 def homepage():
     return render_template("index.html")
+
+@app.route('/gameScreen.html')
+def gameScreen():
+    return render_template("gameScreen.html")
 
 if __name__ == "__main__":
     app.run(use_reloader=True, debug=True)
