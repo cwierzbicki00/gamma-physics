2f847a1e675d5deeb2c6971080e5caa163bfef3e
nickweber <nweber2001@icloud.com>
2023-04-24 11:41:52 -0400

    Add popup object to environment

--- modules/class.environment.js
+++ modules/class.environment.js
@@ -6,79 +6,109 @@
 // description: Contains a base class for an environment object, which is
 //              intended to hold information related to the current level.
 
-
 class Environment {
-    constructor(/*json file*/) {
+  constructor(/*json file*/) {
+    // game objects
+    this.addBoundaries();
+    this.receptacle = new Receptacle("default");
+    this.throwable = new Throwable("tennisball");
+    this.popup = new Popup(200, 200, 200, 200, "test");
+    this.popup.display();
+    this.platforms = [];
+    this.platforms.push(
+      new Platform({ x: width / 4, y: height }, width / 2, 100, 40)
+    );
+    this.platforms.push(new Platform({ x: 400, y: height / 2 }, 200, 100, 0));
 
-        // game objects
-        this.addBoundaries();
-        this.receptacle = new Receptacle('default');
-        this.throwable  = new Throwable('tennisball');
-        this.platforms  = [];
-        this.platforms.push( new Platform({ x: width/4, y: height }, width/2, 100, 40));
-        this.platforms.push( new Platform({ x: 400, y: height/2 }, 200, 100, 0));
+    // TODO awaiting implementation of scoreboard class
+    // this.scoreboard = new scoreboard();
 
-        // TODO awaiting implementation of scoreboard class
-        // this.scoreboard = new scoreboard();
+    // environmental physics
+    this.gravity = createVector(0, 9.8); // earth gravity: 9.8 m/s^2
+    this.wind = createVector(0, 0); // wind force: 0 m/s^2
+    this.backgroundimage = null;
+    // TODO move into class.scoreboard.js
+    // scoreboard variables
+    this.score = 0;
+    this.timer = null;
 
-        // environmental physics
-        this.gravity = createVector(0, 9.8); // earth gravity: 9.8 m/s^2
-        this.wind    = createVector(0, 0);   // wind force: 0 m/s^2
-        this.backgroundimage = null;
-        // TODO move into class.scoreboard.js
-        // scoreboard variables
-        this.score = 0;
-        this.timer = null;
+    this.mouseBarrierActive = true;
 
-        this.mouseBarrierActive = true;
+    console.log("Environment created");
+  }
 
-        console.log("Environment created");
-    }
+  // accessors
+  getThrowable() {
+    return this.throwable;
+  }
+  getReceptacle() {
+    return this.receptacle;
+  }
+  getPlatform(index) {
+    return this.platforms[index];
+  }
+  getGravity() {
+    return this.gravity;
+  }
+  getWind() {
+    return this.wind;
+  }
+  getScore() {
+    return this.score;
+  }
+  getBarrierStatus() {
+    return this.mouseBarrierActive;
+  }
 
-    // accessors
-    getThrowable()     { return this.throwable; }
-    getReceptacle()    { return this.receptacle; }
-    getPlatform(index) { return this.platforms[index]; }
-    getGravity()       { return this.gravity; }
-    getWind()          { return this.wind; }
-    getScore()         { return this.score; }
-    getBarrierStatus() { return this.mouseBarrierActive; }
+  // modifiers
+  setThrowable(newType) {
+    this.throwable = new Throwable(newType);
+  }
+  setReceptacle(newType) {
+    this.receptacle = new Receptacle(newType);
+  }
+  setGravity(newGravity) {
+    this.gravity = createVector(0, newGravity * -1);
+  }
+  setWind(newWindX, newWindY) {
+    this.wind = createVector(newWindX, newWindY);
+  }
+  addScore(newScore) {
+    this.score += newScore;
+  }
+  resetScore() {
+    this.score = 0;
+  }
+  resetTimer(seconds) {
+    this.timer = seconds;
+  }
+  invertBarrierStatus() {
+    this.mouseBarrierActive = !this.mouseBarrierActive;
+  }
 
-    // modifiers
-    setThrowable(newType)       { this.throwable = new Throwable(newType); }
-    setReceptacle(newType)      { this.receptacle = new Receptacle(newType); }
-    setGravity(newGravity)      { this.gravity = createVector(0, newGravity * -1); }
-    setWind(newWindX, newWindY) { this.wind = createVector(newWindX, newWindY); }
-    addScore(newScore)          { this.score += newScore; }
-    resetScore()                { this.score = 0; }
-    resetTimer(seconds)         { this.timer = seconds; }
-    invertBarrierStatus()       { this.mouseBarrierActive = !this.mouseBarrierActive; }
+  update() {
+    this.throwable.update(this);
+    Matter.Engine.update(engine);
+    // this.receptacle.update(this);
 
-    update() {
-        this.throwable.update(this);
-        Matter.Engine.update(engine);
-        // this.receptacle.update(this);
-       
-        // this.scoreboard.update(this);
-    }
-    display() {
+    // this.scoreboard.update(this);
+  }
+  display() {
     this.throwable.display();
     this.receptacle.display();
-    this.platforms.forEach(platform => platform.display(this));
+    this.platforms.forEach((platform) => platform.display(this));
     // this.scoreboard.display(this);
-    }
-    addBoundaries() 
-    {
-                // create the ground
-        const edgeOptions = {
-            isStatic: true,
-            label: 'edges'
-            };
-            let ground = Bodies.rectangle(width / 2, height, width, 5, edgeOptions);
-            let leftWall = Bodies.rectangle(0, height / 2, 5, height, edgeOptions);
-            let rightWall = Bodies.rectangle(width, height / 2, 5, height, edgeOptions);
-            let ceiling = Bodies.rectangle(width / 2, 0, width, 5, edgeOptions);
-            World.add(world, [ground, leftWall, rightWall, ceiling]);
-    }
-
+  }
+  addBoundaries() {
+    // create the ground
+    const edgeOptions = {
+      isStatic: true,
+      label: "edges",
+    };
+    let ground = Bodies.rectangle(width / 2, height, width, 5, edgeOptions);
+    let leftWall = Bodies.rectangle(0, height / 2, 5, height, edgeOptions);
+    let rightWall = Bodies.rectangle(width, height / 2, 5, height, edgeOptions);
+    let ceiling = Bodies.rectangle(width / 2, 0, width, 5, edgeOptions);
+    World.add(world, [ground, leftWall, rightWall, ceiling]);
+  }
 }
--- modules/class.popup.js
+++ modules/class.popup.js
@@ -1,39 +1,38 @@
 //     file name: class.popup.js
 //       authors: Nathan Fleet
 //  date created: 20 Apr 2023
-// date modified: 
+// date modified:
 
 // description: Class for popup which should appear when:
 //                  - Timer runs out
 //                  - Target score is reached
 
-
 // this could likely be implemented in update() Environment class
 class Popup {
-    constructor(x, y, width, height, message) {
-        this.x = x;
-        this.y = y;
-        this.width = width;
-        this.height = height;
-        this.message = message;
-        this.visible = false;
-    }
+  constructor(x, y, width, height, message) {
+    this.x = x;
+    this.y = y;
+    this.width = width;
+    this.height = height;
+    this.message = message;
+    this.visible = true;
+  }
 
-    display() {
-        if(this.visible) {
-            push();
-            rectMode(CENTER);
-            fill(255, 255, 255);
-            rect(this.x, this.y, this.width, this.height);
-            textSize(32);
-            fill(0, 0, 0);
-            textAlign(CENTER, CENTER);
-            text(this.message, this.x, this.y);
-            pop();
-        }
+  display() {
+    if (this.visible) {
+      push();
+      rectMode(CENTER);
+      fill(255, 255, 255);
+      rect(this.x, this.y, this.width, this.height);
+      textSize(32);
+      fill(0, 0, 0);
+      textAlign(CENTER, CENTER);
+      text(this.message, this.x, this.y);
+      pop();
     }
+  }
 
-    hide() {
-        this.visible = false;
-    }
+  hide() {
+    this.visible = false;
+  }
 }
